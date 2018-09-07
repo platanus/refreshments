2ff7884bfbb4e05870a14759efa27f5a291dc8fa
Jose Lezama <joseglego@gmail.com>
2018-09-07 20:15:14 -0300

    WIP: Show QR

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -4,6 +4,7 @@
       <img class="header__logo" src="~/assets/images/platanus_logo.svg">
     </div>
     <div class="app">
+      <qrcode value="lntb147130n1pdexq0xpp5ll527tm2n7sva7lntwurmmant55av3p73n8q04exc2u0d36akwksdqs23jhxapqwfskjmrncqzys0pq6j7rpzemr8l4nw0k7hnrexhxyjslaeg9ekhszvdnfn6qmug79hvulfych20cjzxffdvl238sjrzl0hl8n8md7apt2wpn5xt8s3hsq69k6em" :options="{ size: 200 }"></qrcode>
       <h2>Productos</h2>
       <div class="product-list" key="products">
         <product v-for="product in products" :product="product"></product>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,14 @@
 
 import Vue from 'vue';
 import App from '../app.vue';
+import VueQrcode from '@xkeshi/vue-qrcode';
 
 import store from '../store';
 
 document.addEventListener('DOMContentLoaded', () => {
   if (document.getElementById('app') !== null) {
     const el = document.getElementById('app');
+    Vue.component(VueQrcode.name, VueQrcode);
 
     return new Vue({
       el,
