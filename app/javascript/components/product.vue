875767905085c8a5209456a45be1399174bf7b97
Jose Lezama <joseglego@gmail.com>
2018-09-07 19:29:14 -0300

    chore(product): manage as a component

---
+++ app/javascript/components/product.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="product-list__product" >
+    <img class="product__image" :src="product.image_url">
+    <span class="product__price">$ {{ product.price }}</span>
+    <span class="product__name">{{ product.name }}</span>
+  </div>
+</template>
+<script>
+  import { mapState, mapGetters, mapActions } from 'vuex'
+  export default {
+    props: ['product'],
+    data: function () {
+      return {}
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
