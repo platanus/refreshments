51ff4d47f1085a011e341699101980e9ea8beb6b
oaestay <oaestay@uc.cl>
2018-09-10 00:15:58 -0300

    feat(): Add LightningNetworkClient

---
+++ app/clients/lightning_network_client.rb
@@ -0,0 +1,36 @@
+class LightningNetworkClient
+  include HTTParty
+
+  base_uri ENV.fetch("LN_BASE_URL")
+
+  def invoices
+    self.class.get("/invoices", headers: headers, verify: false)
+  end
+
+  def invoice(r_hash)
+    self.class.get("/invoice/#{r_hash}", headers: headers, verify: false)
+  end
+
+  def create_invoice(memo, satoshis)
+    self.class.post(
+      "/invoices",
+      body: { memo: memo, value: satoshis }.to_json,
+      headers: headers,
+      verify: false
+    )
+  end
+
+  private
+
+  def headers
+    { "Grpc-Metadata-macaroon": macaroon_data }
+  end
+
+  def macaroon_data
+    @macaroon_data ||= ENV.fetch("INVOICE_MACAROON")
+  end
+
+  def config
+    @config ||= LightningNodeConfig.last
+  end
+end
