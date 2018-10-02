aec421c09ea7503ec4be9caa181f08f21f34b00e
Jose Lezama <joseglego@gmail.com>
2018-10-02 17:12:44 -0300

    feat(product): add active

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_153639) do
+ActiveRecord::Schema.define(version: 2018_10_02_200539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2018_09_28_153639) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active", default: true
   end
 
   create_table "users", force: :cascade do |t|
