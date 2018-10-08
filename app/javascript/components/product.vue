590bb8d99738e1badaf848b6f8ce286b4431d5ed
Jose Lezama <joseglego@gmail.com>
2018-10-08 06:53:00 -0300

    feat(product): apply alina-style

--- app/javascript/components/product.vue
+++ app/javascript/components/product.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="product-list__product" >
-    <div class="product__image">
-      <span class="product__price">$ {{ product.price }}</span>
-      <img :src="product.image_url"  @click="incrementProduct(product)">
-      <div class="product__manager" v-if="product.amount > 0">
-        <span class="product-manager__minus" @click="decrementProduct(product)">
-          <font-awesome-icon icon="minus" v-if="product.amount > 1" />
-          <font-awesome-icon icon="trash-alt" v-else />
-        </span>
-        <span class="product-manager__value">{{ product.amount }}</span>
-        <span class="product-manager__plus" @click="incrementProduct(product)">
-          <font-awesome-icon icon="plus" />
-        </span>
-      </div>
-      <div class="product__add" v-else @click="incrementProduct(product)">
-        Agregar
-      </div>
-    </div>
-    <span class="product__name">{{ product.name }}</span>
+    <img class="product__image" :src="product.image_url"  @click="incrementProduct(product)">
   </div>
 </template>
 <script>
@@ -32,8 +15,9 @@
     methods: {
       ...mapActions([
         'decrementProduct',
-        'incrementProduct'
-      ])
+        'incrementProduct',
+        'toggleResume',
+      ]),
     }
   }
 </script>
