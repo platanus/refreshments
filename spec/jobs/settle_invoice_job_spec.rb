13157521be61a0a9abd6d805e7c620b8116a519c
oaestay <oaestay@uc.cl>
2018-10-17 11:01:19 -0300

    feat(jobs): Add SettleInvoiceJob

--- spec/jobs/settle_invoice_job_spec.rb
+++ spec/jobs/settle_invoice_job_spec.rb
@@ -11,6 +11,8 @@ describe SettleInvoiceJob, type: :job do
     SettleInvoiceJob.perform_later(r_hash)
   end
 
+  before { allow_any_instance_of(DoorClient).to receive(:open_door) }
+
   describe "#perform_later" do
     it "enqueues job" do
       expect { perform_later }.to have_enqueued_job
@@ -20,14 +22,27 @@ describe SettleInvoiceJob, type: :job do
   context "with Invoice" do
     let(:invoice) { create(:invoice, r_hash: r_hash, settled: false) }
 
-    it "settles invoice" do
+    it "settles invoice & opens door" do
+      expect_any_instance_of(DoorClient).to receive(:open_door)
       expect { perform }.to change { invoice.reload.settled }.from(false).to(true)
     end
   end
 
-  context "without Invoice" do
-    it "doesn't settle invoice" do
+  context "with settled Invoice" do
+    let(:invoice) { create(:invoice, r_hash: r_hash, settled: true) }
+
+    it "doesn't settle invoice & doesn't open door" do
       expect_any_instance_of(Invoice).not_to receive(:update!)
+      expect_any_instance_of(DoorClient).not_to receive(:open_door)
+      perform
+    end
+  end
+
+  context "without Invoice" do
+    it "doesn't settle invoice & doesn't open door" do
+      expect_any_instance_of(Invoice).not_to receive(:update!)
+      expect_any_instance_of(DoorClient).not_to receive(:open_door)
+      perform
     end
   end
 end
