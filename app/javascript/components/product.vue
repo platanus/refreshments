fe70a4fad5f17e836ebad638ade0c0612aa857a9
Jose Lezama <joseglego@gmail.com>
2018-10-18 01:50:03 -0300

    feat(flash-message): add messages when add/remove product

--- app/javascript/components/product.vue
+++ app/javascript/components/product.vue
@@ -1,10 +1,14 @@
 <template>
-  <v-touch class="product-list__product" v-on:swipeleft="decrementProduct(product)" v-on:swiperight="incrementProduct(product)">
-    <img class="product__image" :src="product.image_url"  @click="incrementProduct(product)">
+  <v-touch class="product-list__product"
+           v-on:swipeleft="decrementProduct(product); message('decrement', product)"
+           v-on:swiperight="incrementProduct(product); message('increment', product)">
+    <img class="product__image" :src="product.image_url"
+         @click="incrementProduct(product); message('increment', product)">
   </v-touch>
 </template>
 <script>
   import { mapActions } from 'vuex';
+  import utils from '../utils/';
 
   export default {
     props: ['product'],
@@ -17,6 +21,13 @@
         'decrementProduct',
         'incrementProduct',
       ]),
+      message(action, product) {
+        const content = utils.contentMessage(action, product);
+
+        if (content.length > 0) {
+          this.flash(content, 'success');
+        }
+      }
     }
   }
 </script>
