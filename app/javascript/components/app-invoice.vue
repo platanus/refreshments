a8fa202080b98a71962caf3073c829d1c148152b
oaestay <oaestay@uc.cl>
2018-10-18 23:43:50 -0300

    hotfix(): Remove spinner

--- app/javascript/components/app-invoice.vue
+++ app/javascript/components/app-invoice.vue
@@ -1,16 +1,5 @@
 <template>
   <div class="invoice">
-    <div class="invoice__loading" v-if="loading">
-      <loading-progress
-        :indeterminate="true"
-        :counter-clockwise="true"
-        :hide-background="true"
-        size="64"
-        rotate
-        fillDuration="2"
-        rotationDuration="1"
-      />
-    </div>
     <div class="invoice__resume">
       <h3 class="invoice__title">Resumen</h3>
       {{ invoice.satoshis || 0 }} Satoshis<br/>
