3699b42b91e1e04c581cf3ecc47688759d32bf3d
ANDRES BANNURA <abannura@uc.cl>
2018-12-18 09:39:04 -0300

    feat(admin):admin can create users through panel

---
+++ app/admin/users.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register User do
+
+  permit_params :email, :password, :password_confirmation
+
+  index do
+    id_column
+    column :email
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  filter :email
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+
+end
