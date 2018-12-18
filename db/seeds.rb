95d971286a33e344c1aa4ac2cefc59d4a7f472b9
ANDRES BANNURA <abannura@uc.cl>
2018-12-18 15:00:15 -0300

    chore(invoiceproducts):populate db with invoice, product and intermediary models

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,24 @@
 # Country.find_or_create_by(name: "Chile") # GOOD
 #
 
-newAdmin = AdminUser.create(email: "admin@example.com", password: "123456")
+new_admin = AdminUser.create(email: "admin@example.com", password: "123456")
+
+new_user = User.create(name: "example_name", email: "user@example.com", password: "123456")
+
+new_product = Product.create(
+  name: "Coca-cola",
+  active: true,
+  price: 500,
+  user: new_user,
+)
+
+new_invoice = Invoice.create(
+  satoshis: 1000,
+  clp: 500,
+  payment_request: "MyString",
+  r_hash: "MyString",
+  memo: "MyString",
+  settled: false,
+)
+
+new_product_invoice = InvoiceProduct.create(product: new_product, invoice: new_invoice)
