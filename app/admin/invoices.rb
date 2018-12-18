35ebb3d1fc0b073986332755eb30eaec8d6106fc
ANDRES BANNURA <abannura@uc.cl>
2018-12-18 16:00:11 -0300

    feat(admin):show product list related to invoice in admin panel

--- app/admin/invoices.rb
+++ app/admin/invoices.rb
@@ -8,4 +8,13 @@ ActiveAdmin.register Invoice do
     column :settled
     actions
   end
+
+  sidebar "Sold products", only: [:show] do
+    table_for invoice.products.order('name ASC') do
+      column "Product" do |product|
+        link_to product.name, [ :admin, product ]
+      end
+      column :price
+    end
+  end
 end
