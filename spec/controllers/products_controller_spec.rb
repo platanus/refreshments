862613d5be24b78c06832b57721c86dfb121f063
ANDRES BANNURA <abannura@uc.cl>
2018-12-20 12:08:15 -0300

    test(activestorage):check that image is uploaded correctly in controller

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -5,127 +5,143 @@ def mock_authentication
   allow(request.env['warden']).to receive(:authenticate!).and_return(user)
 end
 
-def mock_post_request(name, price)
-  post :create, params: { product: { name: name, price: price } }
+def mock_post_request(name, price, image)
+  params = { product: { name: name, price: price } }
+  params[:product][:image] = image unless image.nil?
+  post :create, params: params
 end
 
 RSpec.describe ProductsController, type: :controller do
-  describe "GET #index" do
-    context "unauthenticated user" do
+  describe 'GET #index' do
+    context 'unauthenticated user' do
 
-      it "redirects to sign up form" do
+      it 'redirects to sign up form' do
         get :index
         expect(response).to redirect_to(new_user_session_path)
       end
     end
 
-    context "authenticated user" do
-      let(:user) { create(:user, name: "test_user", email: "test@email.com", password: "123456") }
+    context 'authenticated user' do
+      let(:user) { create(:user) }
       before do
         create_list(:product, 3, user: user)
         mock_authentication
       end
 
-      it "assigns list with user products" do
+      it 'assigns list with user products' do
         get :index
         expect(assigns(:products)).to have(3).items
       end
 
-      it "returns correct 'index' view" do
+      it 'returns correct "index" view' do
         get :index
         expect(response).to render_template('products/index')
       end
     end
   end
 
-  describe "GET #new" do
-    context "unauthenticated user" do
+  describe 'GET #new' do
+    context 'unauthenticated user' do
 
-      it "redirects to sign up form" do
+      it 'redirects to sign up form' do
         get :new
         expect(response).to redirect_to(new_user_session_path)
       end
     end
 
-    context "authenticated user" do
-      let(:user) { create(:user, name: "test_user", email: "test@email.com", password: "123456") }
+    context 'authenticated user' do
+      let(:user) { create(:user) }
       before { mock_authentication }
 
-      it "builds new product" do
+      it 'builds new product' do
         get :new
         expect(assigns(:product)).to be_a_new(Product)
       end
 
-      it "returns correct 'new' view" do
+      it 'returns correct "new" view' do
         get :new
         expect(response).to render_template('products/new')
       end
 
-      it "does not create a product in data base" do
+      it 'does not create a product in data base' do
         get :new
         expect(Product.all.count).to eq(0)
       end
     end
   end
 
-  describe "POST #create" do
-    context "unauthenticated user" do
+  describe 'POST #create' do
+    context 'unauthenticated user' do
 
-      it "redirects to sign up form" do
+      it 'redirects to sign up form' do
         post :create
         expect(response).to redirect_to(new_user_session_path)
       end
     end
 
-    context "authenticated user" do
-      let(:user) { create(:user, name: "test_user", email: "test@email.com", password: "123456") }
+    context 'authenticated user' do
+      let(:user) { create(:user) }
       before { mock_authentication }
 
-      context "with product name, price" do
-        before { mock_post_request("test_product", 100) }
+      context 'with product name, price' do
+        before { mock_post_request('test_product', 100, nil) }
 
-        it "creates a product in database" do
+        it 'creates a product in database' do
           expect(Product.all.count).to eq(1)
         end
 
-        it "creates product with correct name" do
-          expect(assigns(:product)).to have_attributes(name: "test_product")
+        it 'creates product with correct name' do
+          expect(assigns(:product)).to have_attributes(name: 'test_product')
         end
 
-        it "creates product with correct price" do
+        it 'creates product with correct price' do
           expect(assigns(:product)).to have_attributes(price: 100)
         end
       end
 
-      context "with no name" do
-        before { mock_post_request(nil, 100) }
+      context 'with no name' do
+        before { mock_post_request(nil, 100, nil) }
 
-        it "does not create product" do
+        it 'does not create product' do
           expect(Product.all.count).to eq(0)
         end
 
-        it "returns 'new' view" do
+        it 'returns "new" view' do
           expect(response).to render_template('products/new')
         end
 
-        it "sets name error attribute to product" do
-          expect(assigns(:product).errors.messages[:name]).to include("no puede estar en blanco")
+        it 'sets name error attribute to product' do
+          expect(assigns(:product).errors.messages[:name]).to include('no puede estar en blanco')
         end
       end
 
-      context "with no price" do
-        before { mock_post_request("test_name", nil) }
+      context 'with no price' do
+        before { mock_post_request('test_name', nil, nil) }
 
-        it "does not create product" do
+        it 'does not create product' do
           expect(Product.all.count).to eq(0)
         end
 
-        it "returns 'new' view" do
+        it 'returns "new" view' do
           expect(response).to render_template('products/new')
         end
 
-        it "sets name error attribute to product" do
-          expect(assigns(:product).errors.messages[:price]).to include("no puede estar en blanco")
+        it 'sets name error attribute to product' do
+          expect(assigns(:product).errors.messages[:price]).to include('no puede estar en blanco')
+        end
+      end
+
+      context 'with image' do
+        let(:image) do
+          fixture_file_upload(
+            Rails.root.join('spec', 'support', 'assets', 'beverage.jpeg'),
+            'image/jpg'
+          )
+        end
+        before { mock_post_request('test_name', 100, image) }
+
+        it 'uploads image correctly' do
+          expect(assigns(:product).image).to be_attached
         end
       end
     end
