89e7b959fdf70776b103a976bc35eb610b320667
ANDRES BANNURA <abannura@uc.cl>
2018-12-21 10:45:41 -0300

    chore(seeds):add more invoices per product

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,18 +9,6 @@
 # Country.find_or_create_by(name: "Chile") # GOOD
 #
 
-product_names = [
-  "Coca-cola",
-  "Heineken",
-  "Papas fritas",
-  "Skittles",
-  "Popcorn",
-  "Mani",
-  "Manzana",
-  "Platano",
-  "Cafe",
-]
-
 new_admin = AdminUser.create!(email: "admin@example.com", password: "123456")
 
 3.times do |i|
@@ -29,21 +17,24 @@ new_admin = AdminUser.create!(email: "admin@example.com", password: "123456")
     email: "user_#{i}@example.com",
     password: "123456",
   )
-  new_invoice = Invoice.create!(
-    satoshis: 1000,
-    clp: 500,
-    payment_request: "MyString_#{i}",
-    r_hash: "MyString_#{i}",
-    memo: "MyString_#{i}",
-    settled: false,
-  )
-  3.times do |j|
+  4.times do |j|
     new_product = Product.create!(
-      name: product_names[3*i+j],
+      name: "example_name_#{i}_#{j}",
       active: true,
       price: 500,
       user: new_user,
     )
-    new_invoice.invoice_products.create!(product: new_product)
+    5.times do |k|
+      new_invoice = Invoice.create!(
+        satoshis: 1000,
+        clp: 500,
+        payment_request: "MyString_#{i}_#{j}_#{k}",
+        r_hash: "MyString_#{i}_#{j}_#{k}",
+        memo: "MyString_#{i}_#{j}_#{k}",
+        settled: false,
+      )
+      new_invoice.invoice_products.create!(product: new_product)
+    end
   end
 end
+
