0c7f8103c76196ac6bd0465dc94912436f74907f
ANDRES BANNURA <abannura@uc.cl>
2019-01-03 18:44:06 -0300

    feat(buda):add buda client to request for btc withdrawals

--- app/clients/buda_client.rb
+++ app/clients/buda_client.rb
@@ -1,8 +1,96 @@
+require 'base64'
+require 'openssl'
+
 class BudaClient
-  include HTTParty
-  base_uri "https://www.buda.com/api/v2"
+  BASE_URI = ENV.fetch('BUDA_BASE_URI')
+  @@class_lock = Mutex.new
 
   def quotation(market_id:, type:, amount:)
-    self.class.post("/markets/#{market_id}/quotations", body: { type: type, amount: amount })
+    http_method = 'POST'
+    path = "#{BASE_URI}/markets/#{market_id}/quotations"
+    body = create_quotation_request_body(type, amount)
+    send_buda_request(http_method, path, body)
+  end
+
+  def generate_withdrawal(amount, target_address, simulate = false)
+    scope = '/currencies/btc/withdrawals'
+    http_method = 'POST'
+    path = "#{BASE_URI}#{scope}"
+    body = create_withdrawal_request_body(amount, target_address, simulate)
+    headers = headers(http_method, "/api/v2#{scope}", body)
+    send_buda_request(http_method, path, body, headers)
+  end
+
+  private
+
+  def generate_nonce
+    (Time.now.to_f.round(3) * 1000).to_i.to_s
+  end
+
+  def signature(request_type, path, nonce, payload = nil)
+    if payload.nil?
+      "#{request_type} #{path} #{nonce}"
+    else
+      "#{request_type} #{path} #{Base64.strict_encode64(payload)} #{nonce}"
+    end
+  end
+
+  def headers(request_type, path, payload = nil)
+    nonce = generate_nonce
+    encrypted_signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha384'),
+      ENV.fetch('BUDA_API_SECRET'), signature(request_type, path, nonce, payload))
+    {
+      'X-SBTC-APIKEY' => ENV.fetch('BUDA_API_KEY'),
+      'X-SBTC-NONCE' => nonce,
+      'X-SBTC-SIGNATURE' => encrypted_signature,
+      'Content-Type' => 'application/json'
+    }
+  end
+
+  def create_withdrawal_request_body(amount, target_address, simulate)
+    {
+      amount: amount,
+      withdrawal_data: {
+        target_address: target_address
+      },
+      amount_includes_fees: true,
+      simulate: simulate
+    }.to_json
+  end
+
+  def create_quotation_request_body(type, amount)
+    {
+      type: type,
+      amount: amount
+    }.to_json
+  end
+
+  def send_buda_request(http_method, path, body, headers = nil)
+    @@class_lock.synchronize do
+      case http_method
+
+      when 'GET'
+        logger.debug 'HTTP METHOD NOT IMPLEMENTED'
+
+      when 'POST'
+        post_request(path, body, headers)
+
+      when 'PATCH'
+        logger.debug 'HTTP METHOD NOT IMPLEMENTED'
+
+      when 'PUT'
+        logger.debug 'HTTP METHOD NOT IMPLEMENTED'
+
+      when 'DELETE'
+        logger.debug 'HTTP METHOD NOT IMPLEMENTED'
+
+      end
+    end
+  end
+
+  def post_request(path, body, headers)
+    params = { body: body }
+    params[:headers] = headers unless headers.nil?
+    HTTParty.post(path, params)
   end
 end
---
+++ spec/clients/buda_client_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe BudaClient do
+  def initialize_httparty
+    allow(HTTParty).to receive(:post)
+  end
+
+  def initialize_buda_client
+    buda_client = BudaClient.new
+    allow(buda_client).to receive(:create_withdrawal_request_body).and_return('body')
+    allow(buda_client).to receive(:headers).and_return('headers')
+    buda_client.generate_withdrawal(10, 'address', true)
+  end
+
+  def initialize_context
+    initialize_httparty
+    initialize_buda_client
+  end
+
+  context 'generate withdrawall call' do
+    before { initialize_context }
+
+    it 'calls HTTParty with right params' do
+      expect(HTTParty)
+        .to have_received(:post)
+        .with(
+          'https://stg.buda.com/api/v2/currencies/btc/withdrawals',
+          headers: 'headers',
+          body: 'body'
+        )
+    end
+  end
+end
