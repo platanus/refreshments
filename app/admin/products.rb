239ca1e64a35fbab4c6d5e98f4dc1f0b61c29fa3
ANDRES BANNURA <abannura@uc.cl>
2019-01-17 11:51:45 -0300

    feat(admin): update admin product page with regards to new model changes

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,17 +1,14 @@
 ActiveAdmin.register Product do
-  permit_params :name, :price, :image, :active, :user_id
+  permit_params :name, :image
 
   form partial: 'form'
 
   show do
     attributes_table do
       row :name
-      row :price
-      row :active
       row :image do |ad|
         image_tag url_for(ad.image) if ad.image.attached?
       end
-      row :user
     end
   end
 end
