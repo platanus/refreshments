9610f99e0a0730eb265758fe051799ee561402ca
ANDRES BANNURA <abannura@uc.cl>
2019-01-18 16:55:58 -0300

    feat(callback): add after create callback to discount stock automatically on sale

--- spec/models/invoice_product_spec.rb
+++ spec/models/invoice_product_spec.rb
@@ -13,6 +13,12 @@ RSpec.describe InvoiceProduct, type: :model do
     create(:invoice)
   end
 
+  def create_invoice_products(product_invoice_count)
+    product_invoice_count.times do
+      create(:invoice_product, invoice: invoice, user_product: user_product)
+    end
+  end
+
   describe 'basic validations' do
     it { should belong_to(:user_product) }
     it { should belong_to(:invoice) }
@@ -24,13 +30,30 @@ RSpec.describe InvoiceProduct, type: :model do
     end
   end
 
-  describe "before validations 'fix_product_price' hook" do
-    let(:invoice) { initialize_invoice }
-    let(:user_product) { create(:user_product) }
-    let(:invoice_product) { create(:invoice_product, invoice: invoice, user_product: user_product) }
+  let(:invoice) { initialize_invoice }
+  let(:user_product) { create(:user_product, stock: 3) }
+  let(:invoice_product) { create(:invoice_product, invoice: invoice, user_product: user_product) }
 
+  describe "before validations 'fix_product_price' hook" do
     it 'saves invoice product with correct price' do
       expect(invoice_product.product_price).to eq(user_product.price * MOCKED_SATOSHI_CLP_RATIO)
+    end
+  end
+
+  describe '#discount_stock' do
+    it 'substracts 1 unit from user product stock' do
+      expect { create_invoice_products(1) }.to change { user_product.stock }.by(-1)
+    end
+
+    it 'creates 3 invoice products and stock reaches 0' do
+      create_invoice_products(3)
+      expect(user_product.stock).to eq(0)
+    end
+
+    it 'raises error due to no stock' do
+      expect { create_invoice_products(4) }.to raise_error(ActiveRecord::RecordInvalid)
+      expect(UserProduct.first.stock).to eq(0)
+      expect(InvoiceProduct.all.count).to eq(3)
     end
   end
 end
