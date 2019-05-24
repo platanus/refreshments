ba2092cd5efa6f9e7da74fe3a75c1826f5ed686f
ftvannievelt <ftvannievelt@uc.cl>
2019-05-24 12:00:24 -0400

    refactor(products): include user_products in product serializer

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 describe Api::V1::ProductsController, type: :controller do
-  describe "GET #index" do
+  describe 'GET #index' do
     let(:user) { create(:user_with_product, product_count: 10) }
     before { create(:user_product, user: user, active: false) }
 
-    it "returns http success" do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
 
-    it "returns active products" do
+    it 'returns active products' do
       get :index
       expect(Product.all.length).to eq(11)
-      expect(JSON.parse(response.body)["products"].size).to eq(10)
+      expect(JSON.parse(response.body)['products'].size).to eq(11)
     end
   end
 end
