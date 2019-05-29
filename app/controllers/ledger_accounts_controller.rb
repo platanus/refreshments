f9da62b5b3a7e67466c21431aef6c090d364db83
ftvannievelt <ftvannievelt@uc.cl>
2019-05-29 13:29:13 -0400

    refactor(ledger-accounts): use joins to import product data

--- app/controllers/ledger_accounts_controller.rb
+++ app/controllers/ledger_accounts_controller.rb
@@ -2,22 +2,26 @@ class LedgerAccountsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @ledger_account = LedgerAccount.find_by_accountable_id(current_user.id)
+    @ledger_account = LedgerAccount.find_by(accountable_id: current_user.id)
     @ledger_lines = LedgerLine.where(ledger_account_id: @ledger_account.id).sorted_desc
     @total_balance = @ledger_lines.empty? ? 0 : @ledger_lines.first.balance
-    line_id_to_product_data_hash
+    @line_id_to_product_data_hash = line_id_to_product_data_hash
   end
 
   private
 
   def line_id_to_product_data_hash
-    sales_lines = @ledger_lines.where(accountable_type: 'InvoiceProduct')
-    @line_id_to_product_data_hash = sales_lines.map { |line| import_product_data(line) }.to_h
+    @ledger_lines
+      .where(accountable_type: 'InvoiceProduct')
+      .map { |line| import_product_data(line) }
+      .to_h
   end
 
   def import_product_data(line)
-    invoice_product = InvoiceProduct.find(line.accountable_id)
-    user_product = UserProduct.find(invoice_product.user_product_id)
+    user_product = UserProduct
+                   .joins(:invoice_products)
+                   .where(invoice_products: { id: line.accountable_id })
+                   .last
     product = Product.find(user_product.product_id)
     [line.id, [product.name, user_product.price]]
   end
