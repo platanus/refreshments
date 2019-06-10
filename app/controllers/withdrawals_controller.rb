2f07479ffc8b44995b7e81a187fcc3db465a2ba6
ftvannievelt <ftvannievelt@uc.cl>
2019-06-10 11:36:16 -0400

    feat(withdrawals): remove withdrawals index

--- app/controllers/withdrawals_controller.rb
+++ app/controllers/withdrawals_controller.rb
@@ -1,12 +1,6 @@
 class WithdrawalsController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @withdrawals = current_user.withdrawals
-    @total_confirmed = current_user.total_confirmed_withdrawals
-    @total_pending = current_user.total_pending_withdrawals
-  end
-
   def new
     @withdrawal = current_user.withdrawals.new
   end
