006b54c0304193c3e86361902fca186281be443f
ftvannievelt <ftvannievelt@uc.cl>
2019-06-11 12:26:23 -0400

    refactor(create-invoice): change invoice controller and api call to use shopping_cart_items

--- spec/controllers/api/v1/invoices_controller_spec.rb
+++ spec/controllers/api/v1/invoices_controller_spec.rb
@@ -4,31 +4,34 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
   describe 'POST #create' do
     let(:products_hash) do
       {
-        '1' => { 'amount' => 3, 'price' => 1000 },
-        '2' => { 'amount' => 2, 'price' => 1000 }
+        123 => { 'amount' => 3 },
+        234 => { 'amount' => 2 }
       }
     end
-
     let(:invoice_params) do
-      {
-        invoice: {
-          memo: 'Guillermo Moreno',
-          products: products_hash
-        }
-      }
+      { invoice: { products: products_hash } }
     end
+    let!(:product_1) { create(:product, id: 123) }
+    let!(:product_2) { create(:product, id: 234) }
 
-    before { allow(CreateInvoice).to receive(:for) }
+    before do
+      expect(CreateInvoice).to receive(:for)
+    end
 
     it 'returns http success' do
       post :create, params: invoice_params, as: :json
       expect(response).to have_http_status(:success)
     end
 
-    it 'calls CreateInvoice' do
+    it 'shopping_cart_items have correct values' do
       post :create, params: invoice_params, as: :json
-      expect(CreateInvoice).to have_received(:for).with(
-        memo: 'Guillermo Moreno', products_hash: products_hash
+      expect(assigns(:shopping_cart_items).first).to have_attributes(
+        product: product_1,
+        amount: 3
+      )
+      expect(assigns(:shopping_cart_items).last).to have_attributes(
+        product: product_2,
+        amount: 2
       )
     end
   end
