147d3d43217dbb57cb190bd165d5d460fc494c6a
ftvannievelt <ftvannievelt@uc.cl>
2019-06-28 13:53:31 -0400

    feat(lightning-network-withdrawal): add lightning_network_withdrawal controller

---
+++ app/controllers/lightning_network_withdrawals_controller.rb
@@ -0,0 +1,22 @@
+class LightningNetworkWithdrawalsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @lightning_network_withdrawal = LightningNetworkWithdrawal.new
+  end
+
+  def create
+    PayInvoiceToUser.for(
+      invoice_hash: lightning_network_withdrawal_params[:invoice_hash],
+      user: current_user
+    )
+
+    render 'feedback'
+  end
+
+  private
+
+  def lightning_network_withdrawal_params
+    params.require(:lightning_network_withdrawal).permit(:invoice_hash)
+  end
+end
