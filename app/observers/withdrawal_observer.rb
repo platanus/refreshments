cabd272d745f2d1e67fc3f54f0ed7e68352e1c52
Jaime Bünzli <jpbunzli@gmail.com>
2019-07-19 12:46:20 -0400

    chore(withdrawals): remove useless onchain withdrawal observer

--- app/observers/withdrawal_observer.rb
+++
@@ -1,7 +0,0 @@
-class WithdrawalObserver < PowerTypes::Observer
-  after_save :register_withdrawal_payment
-
-  def register_withdrawal_payment
-    RegisterWithdrawalPaymentJob.set(wait: 5.seconds).perform_later(object)
-  end
-end
