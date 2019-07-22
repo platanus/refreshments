604013a5ade0d7b4069528f36e616c7e59d3e4dc
isidoravs <isvizcaya@uc.cl>
2019-07-22 16:37:15 -0400

    fix(ledger-line): order id asc

--- app/models/ledger_line.rb
+++ app/models/ledger_line.rb
@@ -4,7 +4,7 @@ class LedgerLine < ApplicationRecord
 
   scope :before_date, ->(date) { where("date < ?", date) }
   scope :after_date, ->(date) { where("date >= ?", date) }
-  scope :sorted, -> { order(date: :asc, id: :desc) }
+  scope :sorted, -> { order(date: :asc, id: :asc) }
   scope :sorted_desc, -> { order(date: :desc, id: :desc) }
 end
 
