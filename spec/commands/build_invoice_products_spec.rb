d4b38aa7327fcdde7ad79f5a2e5162efb18d3a3b
isidoravs <isvizcaya@uc.cl>
2019-07-24 17:20:15 -0400

    refactor(invoice): build invoice products with user_product as shopping_cart_item

--- spec/commands/build_invoice_products_spec.rb
+++ spec/commands/build_invoice_products_spec.rb
@@ -2,59 +2,40 @@ require 'rails_helper'
 
 describe BuildInvoiceProducts do
   let(:amount) { 1 }
-  let(:product) { create(:product) }
-  let(:shopping_cart_item) { ShoppingCartItem.new(product.id, amount) }
 
-  def perform
-    described_class.for(shopping_cart_item: shopping_cart_item)
+  def set_shopping_cart_item(user_product)
+    @shopping_cart_item = ShoppingCartItem.new(user_product.id, amount)
   end
 
-  context "with amount = 1 and only 1 user_product with stock for this product" do
+  def perform
+    described_class.for(shopping_cart_item: @shopping_cart_item)
+  end
+
+  context "with amount = 1 and user_product with stock for this product" do
     before do
-      @user_product = create(:user_product, product: product, stock: 10)
+      @user_product = create(:user_product, stock: 10)
+      set_shopping_cart_item(@user_product)
     end
 
     it { expect(perform.first.user_product).to eq(@user_product) }
     it { expect(perform.first.product_price).to eq(@user_product.price) }
   end
 
-  context "with amount = 1 and only 1 user_product without stock for this product" do
+  context "with amount = 1 and user_product without stock for this product" do
     before do
-      @user_product = create(:user_product, product: product, stock: 0)
+      @user_product = create(:user_product, stock: 0)
+      set_shopping_cart_item(@user_product)
     end
 
     it { expect(perform.count).to eq(0) }
   end
 
-  context "with amount = 1 and only no user_product for this product" do
-    it { expect(perform.count).to eq(0) }
-  end
-
-  context "with amount = 1 and 2 user_product with stock and different price" do
-    before do
-      @user_product = create(:user_product, product: product, stock: 10, price: 2000)
-      @best_user_product = create(:user_product, product: product, stock: 10, price: 1000)
-    end
-
-    it { expect(perform.first.user_product).to eq(@best_user_product) }
-    it { expect(perform.first.product_price).to eq(@best_user_product.price) }
-  end
-
-  context "with amount = 1 and 2 user_product and the cheapest has no stock" do
-    before do
-      @best_user_product = create(:user_product, product: product, stock: 10, price: 2000)
-      @user_product = create(:user_product, product: product, stock: 0, price: 1000)
-    end
-
-    it { expect(perform.first.user_product).to eq(@best_user_product) }
-    it { expect(perform.first.product_price).to eq(@best_user_product.price) }
-  end
-
-  context "with amount = 2 and user_product for this product has 1 item in stock" do
+  context "with amount = 2 and user_product has 1 item in stock" do
     let(:amount) { 2 }
 
     before do
-      @user_product = create(:user_product, product: product, stock: 1, price: 1000)
+      @user_product = create(:user_product, stock: 1, price: 1000)
+      set_shopping_cart_item(@user_product)
     end
 
     it { expect(perform.count).to eq(0) }
@@ -64,7 +45,8 @@ describe BuildInvoiceProducts do
     let(:amount) { 2 }
 
     before do
-      @user_product = create(:user_product, product: product, stock: 2, price: 1000)
+      @user_product = create(:user_product, stock: 2, price: 1000)
+      set_shopping_cart_item(@user_product)
     end
 
     it { expect(perform.count).to eq(2) }
