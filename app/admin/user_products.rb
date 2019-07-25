83cf767680baff596e10632402e5fb931f62a5c5
isidoravs <isvizcaya@uc.cl>
2019-07-25 16:54:15 -0400

    refactor(admin): remove reference to product from admin

--- app/admin/user_products.rb
+++ app/admin/user_products.rb
@@ -1,15 +1,18 @@
 ActiveAdmin.register UserProduct do
-  permit_params :price, :user_id, :product_id, :active, :stock
+  permit_params :name, :price, :user_id, :active, :stock, :image
 
   form partial: 'form'
 
   show do
     attributes_table do
+      row :name
       row :user
-      row :product
       row :price
       row :active
       row :stock
+      row :image do |ad|
+        image_tag url_for(ad.image) if ad.image.attached?
+      end
     end
   end
 end
