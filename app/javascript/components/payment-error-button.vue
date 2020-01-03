b209d58fa06770ea29fbceba6040829813ba9fd9
kyuubinokitsune <pijadue@uc.cl>
2020-01-03 17:50:25 -0300

    feat(ui/notWorkingButtonDelay): button appears 10 secs after QR is loads

---
+++ app/javascript/components/payment-error-button.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="payment-error-button">
+    <modal name="apology-modal">
+      <div class="invoice__apology-modal-text">
+        <h1>Error en el pago</h1>
+        Disculpe las molestias :(, lo vamos a revisar para que puedas pagar en el futuro
+      </div>
+    </modal>
+    <div class="invoice__error-btn ">
+      <button
+        type="button"
+        v-if="showApologyButton"
+        class="btn"
+        @click="show"
+      >
+        ¿No funciona?
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import invoiceApi from '../api/invoices';
+
+const SHOW_APOLOGY_BUTTON = 10000;
+const CLOSE_MODAL_AFTER_IDLE_WAIT = 7000;
+
+export default {
+  data() {
+    return {
+      showApologyButton: false,
+    };
+  },
+  methods: {
+    show() {
+      this.$modal.show('apology-modal');
+      invoiceApi.notifyPaymentError();
+      setTimeout(() => {
+        this.$modal.hide('apology-modal');
+      }, CLOSE_MODAL_AFTER_IDLE_WAIT);
+    },
+    hide() {
+      this.$modal.hide('apology-modal');
+    },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showApologyButton = true;
+    }, SHOW_APOLOGY_BUTTON);
+  },
+};
+</script>
