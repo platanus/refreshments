a09332a06f915ea0c0726e331a90e68a6a81cdf6
kyuubinokitsune <pijadue@uc.cl>
2020-01-07 16:08:35 -0300

    fix(ui/buttonDoesNotAppearAfterReload): payment not working button does not appear after reload

--- app/javascript/components/app-invoice.vue
+++ app/javascript/components/app-invoice.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="invoice">
-    <errorNotification v-if="!loading" />
+    <errorNotification v-if="!loading && invoice.paymentRequest" />
     <div
       class="invoice__info"
       v-if="totalPrice > 0"
