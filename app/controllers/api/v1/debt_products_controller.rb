3c4d8380ef3929c6df3896b6501086f294f95913
kyuubinokitsune <pijadue@uc.cl>
2020-01-17 11:31:09 -0300

    (feat/DebtActiveAdmin): debts appear on administration dashboard

--- app/controllers/api/v1/debt_products_controller.rb
+++ app/controllers/api/v1/debt_products_controller.rb
@@ -2,13 +2,11 @@ class Api::V1::DebtProductsController < Api::V1::BaseController
   def create_debt_product
     debtor = create_params[:debtor]
     create_params[:products].each do |product|
-      debt = DebtProduct.create(
+      DebtProduct.create(
       debtor: debtor,
       product_id: product[:product_id],
       product_price: product[:product_price]
     )
-    puts 'Abajo la deuda guardada'
-    puts debt
     end
   end
 
